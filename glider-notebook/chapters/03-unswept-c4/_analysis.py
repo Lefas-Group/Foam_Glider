12794f6344ec475c010a9cc9500f3d604e2177cb
Oliver932 <76821440+Oliver932@users.noreply.github.com>
2026-09-14 23:06:19 +0100

    What happens if we keep the quarter chord constant?
    Asked: what if we keep the quarter chord constant?

---
+++ glider-notebook/chapters/03-unswept-c4/_analysis.py
@@ -0,0 +1,38 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+
+def optimize_glider_unswept_c4(verbose=False):
+    opti = asb.Opti()
+    c_root = opti.variable(init_guess=0.08, lower_bound=0.01, upper_bound=0.15)
+    taper = opti.variable(init_guess=0.5, lower_bound=0.1, upper_bound=1.0)
+    alpha = opti.variable(init_guess=5, lower_bound=-5, upper_bound=15)
+    V = opti.variable(init_guess=3, lower_bound=1, upper_bound=20)
+    gamma = opti.variable(init_guess=-5, lower_bound=-45, upper_bound=45)
+    cg_x = opti.variable(init_guess=0.03, lower_bound=-0.05, upper_bound=0.15)
+
+    # The vehicle function is already in scope because both files are exec'd into the notebook
+    airplane = make_glider_unswept_c4(c_root, taper, cg_x)
+    aero = asb.AeroBuildup(airplane=airplane, op_point=asb.OperatingPoint(velocity=V, alpha=alpha)).run()
+
+    wings_area = airplane.wings[0].area() + airplane.wings[1].area() + airplane.wings[2].area()
+    fuse_mass = (0.2 * 0.015) * 2 * 0.1744 
+    mass = wings_area * 0.1744 + fuse_mass
+    weight = mass * 9.81
+
+    opti.subject_to([
+        aero["L"] == weight * np.cosd(gamma),
+        aero["D"] == weight * np.sind(-gamma),
+        aero["Cm"] == 0
+    ])
+
+    sink_rate = V * np.sind(-gamma)
+    opti.minimize(sink_rate)
+    sol = opti.solve(verbose=verbose)
+    
+    return {
+        "sol": sol,
+        "sink_rate": sol.value(sink_rate),
+        "c_root": sol.value(c_root),
+        "taper": sol.value(taper),
+        "cg_x": sol.value(cg_x)
+    }
---
+++ glider-notebook/chapters/03-unswept-c4/_model.py
@@ -0,0 +1,36 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+
+def make_glider_unswept_c4(c_root, taper, cg_x):
+    # Tip LE x such that quarter chord x is constant
+    tip_le_x = 0.25 * c_root * (1 - taper)
+    
+    wing = asb.Wing(
+        name="Wing", symmetric=True,
+        xsecs=[
+            asb.WingXSec(xyz_le=[0, 0, 0], chord=c_root, airfoil=asb.Airfoil("naca4405")),
+            asb.WingXSec(xyz_le=[tip_le_x, 0.15, 0], chord=c_root * taper, airfoil=asb.Airfoil("naca4405"))
+        ]
+    )
+    htail = asb.Wing(
+        name="H-tail", symmetric=True,
+        xsecs=[
+            asb.WingXSec(xyz_le=[0.15, 0, 0], chord=0.03, airfoil=asb.Airfoil("naca0005")),
+            asb.WingXSec(xyz_le=[0.15, 0.05, 0], chord=0.03, airfoil=asb.Airfoil("naca0005"))
+        ]
+    )
+    vtail = asb.Wing(
+        name="V-tail", symmetric=False,
+        xsecs=[
+            asb.WingXSec(xyz_le=[0.15, 0, 0], chord=0.03, airfoil=asb.Airfoil("naca0005")),
+            asb.WingXSec(xyz_le=[0.15, 0, 0.05], chord=0.03, airfoil=asb.Airfoil("naca0005"))
+        ]
+    )
+    fuse = asb.Fuselage(
+        name="Fuselage",
+        xsecs=[
+            asb.FuselageXSec(xyz_c=[-0.02, 0, 0], width=0.01, height=0.015, shape=4),
+            asb.FuselageXSec(xyz_c=[0.18, 0, 0], width=0.01, height=0.015, shape=4)
+        ]
+    )
+    return asb.Airplane(name="Glider", xyz_ref=[cg_x, 0, 0], wings=[wing, htail, vtail], fuselages=[fuse])
